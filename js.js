d268c2e7d9cdf486325991df4518b4f3405c2fbb
diosdefin <danielaybekuulu@gmail.com>
2024-04-23 01:44:40 +0600

    адаптация бутту calc()

--- js.js
+++ js.js
@@ -1,11 +1,34 @@
-document.addEventListener('keydown', function(event) {
-    const key = event.key.toUpperCase();
-    const keyboardKey = document.querySelector(`.keyboard-key:contains('${key}')`);
-    
-    if (keyboardKey) {
-        keyboardKey.classList.add('active');
-        setTimeout(function() {
-            keyboardKey.classList.remove('active');
-        }, 300);
+window.addEventListener('keydown', function(event) {
+    var key = event.key.toUpperCase();
+    var keyElements = document.querySelectorAll('.keyboard-key');
+    keyElements.forEach(function(keyElement) {
+        if (keyElement.textContent === key) {
+            keyElement.classList.add('color_key');
+        }
+    });
+});
+
+window.addEventListener('keyup', function(event) {
+    var key = event.key.toUpperCase();
+    var keyElements = document.querySelectorAll('.keyboard-key');
+    keyElements.forEach(function(keyElement) {
+        if (keyElement.textContent === key) {
+            keyElement.classList.remove('color_key');
+        }
+    });
+});
+
+window.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        var spaceElement = document.querySelector('.keyboard-tab');
+        spaceElement.classList.add('color_key');
     }
 });
+
+window.addEventListener('keyup', function(event) {
+    if (event.code === 'Space') {
+        var spaceElement = document.querySelector('.keyboard-tab');
+        spaceElement.classList.remove('color_key');
+    }
+});
+
