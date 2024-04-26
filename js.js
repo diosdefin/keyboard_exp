0e4693baa50fca03ae437660315bb2b6a76a683b
diosdefin <danielaybekuulu@gmail.com>
2024-04-26 14:35:22 +0600

    nich

--- js.js
+++ js.js
@@ -7,7 +7,6 @@ window.addEventListener('keydown', function(event) {
         }
     });
 });
-
 window.addEventListener('keyup', function(event) {
     var key = event.key.toUpperCase();
     var keyElements = document.querySelectorAll('.keyboard-key');
@@ -17,14 +16,12 @@ window.addEventListener('keyup', function(event) {
         }
     });
 });
-
 window.addEventListener('keydown', function(event) {
     if (event.code === 'Space') {
         var spaceElement = document.querySelector('.keyboard-tab');
         spaceElement.classList.add('color_key');
     }
 });
-
 window.addEventListener('keyup', function(event) {
     if (event.code === 'Space') {
         var spaceElement = document.querySelector('.keyboard-tab');
@@ -32,3 +29,17 @@ window.addEventListener('keyup', function(event) {
     }
 });
 
+const hed = document.querySelector('header')
+const main = document.querySelector('main')
+const key = document.getElementsByClassName('keyboard-key')
+const tab = document.getElementsByClassName('keyboard-tab')
+const btn = document.getElementById('ik')
+const body = document.body
+btn.addEventListener('click',() =>{
+    hed.classList.toggle('header_1')
+    main.classList.toggle('main_1')
+    btn.classList.toggle('ik_1')
+    body.classList.toggle('body_1')
+    key.classList.toggle('keyboardkey_1')
+    tab.classList.toggle('keyboardtab_1')
+})
